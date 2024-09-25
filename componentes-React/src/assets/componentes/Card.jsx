1dc4d1c4c8e70d6a233a967b186a16daeec406eb
MARIA PAULA LLANO BRAVO <maria.llano@uao.edu.co>
2024-09-24 21:11:38 -0500

    react

--- componentes-React/src/assets/componentes/Card.jsx
+++ componentes-React/src/assets/componentes/Card.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import "./Card.css"
 
-const Card = () => {
+const Card = ({lang,img}) => {
   return (
+    
     <>
-    <div className='estiloCard'> 
-        <img src="https://i.etsystatic.com/25230713/r/il/1fa4c4/5757009313/il_fullxfull.5757009313_aluq.jpg " alt=" " />
+    <div className='estiloCard' style={{
+        background: "linear-gradient(to left ${fcolor},${scolor})"
+    }}> 
+        <img src={img} alt=" " />
         <h1>Holiwi</h1>
-        <h3>perrito</h3>
+        <h3>{lang}</h3>
     </div>
     </>
   )
